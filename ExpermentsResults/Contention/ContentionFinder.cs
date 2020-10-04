7323a8911d718a73af8c1e825e31c699faf8c4c1
Ammar Hawbani <33313489+howbani@users.noreply.github.com>
2020-10-04 17:48:35 +0800

    Add files via upload

---
+++ ExpermentsResults/Contention/ContentionFinder.cs
@@ -0,0 +1,22 @@
+﻿using LBDD.Dataplane.NOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBDD.ExpermentsResults.Contention
+{
+    /// <summary>
+    /// how many nodes are envolved when data packets are routed.
+    /// smaller number of nodes means less contention.
+    /// </summary>
+    class ContentionFinder
+    {
+        // 
+        public double FineContentionM(List<Packet> packets)
+        {
+            return 0;
+        }
+    }
+}
---
+++ db/UiImportTopology.xaml.cs
@@ -0,0 +1,26 @@
+﻿using LBDD.Dataplane;
+using LBDD.ui;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LBDD.db
+{
+    /// <summary>
+    /// Interaction logic for UiImportTopology.xaml
+    /// </summary>
+    public partial class UiImportTopology : Window
+    {
+
+        public UiImportTopology(MainWindow MainWindow)
+        {
+            InitializeComponent();
+
+           foreach( NetwokImport net in NetworkTopolgy.ImportNetworkNames(this))
+           {
+                net.MainWindow = MainWindow;
+                stk_netwoks.Children.Add(net);
+           }
+        }
+    }
+}
